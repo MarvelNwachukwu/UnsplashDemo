9796be811b73d48c2a91a4cad73ff86eece253dd
MarvelNwachukwu <marvellousn561@gmail.com>
2020-06-15 22:08:41 +0100

    disabled JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,18 @@
 
-const buttons = document.querySelectorAll('button');
-var input;
-var $1stInput;
-var $2ndInput;
-let processedInput = [''];
+// const buttons = document.querySelectorAll('button');
+// var input;
+// var $1stInput;
+// var $2ndInput;
+// let processedInput = [''];
 
-var getCurrentInput = setInterval(() => {
-    input = document.querySelectorAll('input');
-    $1stInput = document.querySelector('#SearchImage');
-    $2ndInput = document.querySelector('#SearchForImages');
-    $1stInputValue = $1stInput.value;
-    $2ndInputValue = $2ndInput.value;
+// var getCurrentInput = setInterval(() => {
+//     input = document.querySelectorAll('input');
+//     $1stInput = document.querySelector('#SearchImage');
+//     $2ndInput = document.querySelector('#SearchForImages');
+//     $1stInputValue = $1stInput.value;
+//     $2ndInputValue = $2ndInput.value;
 
-    return [$1stInput, $2ndInput, $1stInputValue, $2ndInputValue]
-}, 500);
+//     return [$1stInput, $2ndInput, $1stInputValue, $2ndInputValue]
+// }, 500);
 
-let values = getCurrentInput();
+// let values = getCurrentInput();
